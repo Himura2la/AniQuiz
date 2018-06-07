a67a9bdd564b008a1702ac160e3bd5a0feec2252
Himura Kazuto <glagol15@gmail.com>
2018-06-08 01:18:07 +0300

    lib init rework

--- hard/lib-init.bat
+++ hard/lib-init.bat
@@ -1,19 +1,15 @@
-@echo off
-
-cd ..
-cd lib
-
 git submodule init
 git submodule update
 
-cd ..
+@echo off
+@echo.
 
 (
 echo ^(fp_lib_table
-echo   ^(lib ^(name ESP8266^)^(type KiCad^)^(uri %cd%\lib\ESP8266.pretty^)^(options ""^)^(descr ""^)^)
-echo   ^(lib ^(name Project-Lib^)^(type KiCad^)^(uri %cd%\proj-lib.pretty^)^(options ""^)^(descr ""^)^)
+echo   ^(lib ^(name ESP8266^)^(type KiCad^)^(uri %~dp0%lib-ESP8266\ESP8266.pretty^)^(options ""^)^(descr ""^)^)
+echo   ^(lib ^(name Project-Lib^)^(type KiCad^)^(uri %~dp0%lib.pretty^)^(options ""^)^(descr ""^)^)
 echo ^)
-) > "%~dp0%fp-lib-table"
+) > "%~dp0%proj\fp-lib-table"
 
 
 echo 4. To make the 3d models usable, add the location from step #1 to list
@@ -23,7 +19,7 @@ echo.
 echo        kicad:Preferences-^>Configure Paths-^>Add
 echo.
 echo            Name: ESPLIB
-echo            Path: %cd%
+echo            Path: %~dp0%lib-ESP8266
 echo.
 
 
